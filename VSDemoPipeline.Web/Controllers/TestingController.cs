8fe406e0d2e32d8455c811d5ba9393fc7b9dc4c6
Ramesh Raghunathan <ramesh16202@outlook.com>
2021-03-20 18:34:27 +0530

    removed error code from testing controller

--- VSDemoPipeline.Web/Controllers/TestingController.cs
+++ VSDemoPipeline.Web/Controllers/TestingController.cs
@@ -10,7 +10,7 @@ namespace VSDemoPipeline.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View("This is testing");
+            return View();
         }
     }
 }
